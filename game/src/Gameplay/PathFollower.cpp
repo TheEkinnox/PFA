5fc6075894735f47cf847c833cebdac46611ec5c
amadou-design <acisse291004@gmail.com>
2023-04-17 15:17:53 -0400

    PathFollower is now updated

--- game/src/Gameplay/PathFollower.cpp
+++ game/src/Gameplay/PathFollower.cpp
@@ -1,37 +1,48 @@
 #include"Gameplay/PathFollower.h"
+#include <Utility/ServiceLocator.h>
 
 using namespace PFA::Gameplay;
 using Vec3 = LibMath::Vector3;
 
-PathFollower::PathFollower(const std::vector<Vec3>& p_path, ECyclingMode p_cyclingMode = ECyclingMode::LOOP)
+
+
+PFA::Gameplay::PathFollower::PathFollower(LibGL::Entity& p_owner, const std::vector<Vec3>& p_path, ECyclingMode p_cyclingMode, float p_moveSpeed):
+	LibGL::Component( p_owner),
+		m_path(p_path),
+		m_currentIndex(0),
+		m_nextIndex(1),
+		m_cyclingMode(p_cyclingMode),
+		m_moveSpeed(p_moveSpeed)
 {
-	m_path = p_path;
-	m_currentIndex = 0;
-	m_cyclingMode = p_cyclingMode;
+	
 }
 
-const Vec3& PathFollower::currentPoint()
+Vec3 PathFollower::currentPoint()const
 {
 	return m_path[m_currentIndex];
 }
 
-const Vec3& PathFollower::nextPoint()
+ Vec3 PathFollower::nextPoint() const
 {
-	size_t nextIndex; 
+	 return m_path[m_nextIndex];
+}
+
+void PFA::Gameplay::PathFollower::computeNext()
+{
 	if (m_currentIndex == m_path.size() - 1)
 	{
 		switch (m_cyclingMode)
 		{
 		case ECyclingMode::LOOP:
 
-			nextIndex = 0;
+			m_nextIndex = 0;
 			break;
 
 		case ECyclingMode::PING_PONG:
 
 			std::reverse(m_path.begin(), m_path.end());
 			m_currentIndex = 0;
-			nextIndex = 1;
+			m_nextIndex = 1;
 
 			break;
 
@@ -41,15 +52,30 @@ const Vec3& PathFollower::nextPoint()
 	}
 	else
 	{
-		nextIndex = m_currentIndex + 1;
+		m_nextIndex = m_currentIndex + 1;
 	}
-
-	m_currentIndex = nextIndex;
-
-	return m_path[nextIndex];
 }
 
 void PathFollower::update()
 {
-	nextPoint();
+	float deltaTime = LGL_SERVICE(LibGL::Utility::Timer).getDeltaTime();
+
+	if (!isActive())
+		return;
+
+	const Vec3& current = currentPoint();
+	const Vec3& next = nextPoint();
+
+	Vec3 toNext = next - getOwner().getPosition();
+	float distanceToNext = toNext.magnitude();
+
+	if (distanceToNext < m_moveSpeed)
+	{
+		m_currentIndex = m_nextIndex;
+		computeNext();
+	}
+	else
+	{
+		getOwner().translate(toNext.normalized() * m_moveSpeed * deltaTime);
+	}
 }
