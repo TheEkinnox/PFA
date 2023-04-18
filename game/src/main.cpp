5daa2efca0b801b9b81813b8ca1b38fc88a71874
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2023-04-18 18:45:06 -0400

    Removed audio test from main

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,30 +1,16 @@
 #include "Core/GameApp.h"
 
 #include <crtdbg.h>
-#include <irrKlang.h>
-using namespace irrklang;
 
 int main()
 {
-//#ifdef _DEBUG
-//	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-//#endif
-//
-//	PFA::Core::GameApp app;
-//
-//	app.run();
-	ISoundEngine* engine = createIrrKlangDevice();
-	if (!engine)
-	{
-		return 0;
-	}
+#ifdef _DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 
-	engine->play2D("media/getout.ogg", true);
+	PFA::Core::GameApp app;
 
-	while (true)
-	{
+	app.run();
 
-	}
-	engine->drop();
 	return 0;
 }
